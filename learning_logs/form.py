594f287a4a96ffff708a3916f2c4df62e8658379
starryhwj <241964143@qq.com>
2019-07-26 14:47:32 +0800

    Initial commit

---
+++ learning_logs/form.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# filename: form.py
+
+from django import forms
+from .models import Topic, Entry
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = {'text': ''}
+
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# filename: users/urls.py
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+from . import views
+
+urlpatterns = [
+    url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^register/$', views.register, name='register'),
+]
